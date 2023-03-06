292f5c0796241691398c7887d55265f3ad0d68da
guillermopalazoncano <guillermopalazoncano@localhost>
2023-03-06 22:47:52 +0100

    Nuevo métodos resta y multiplica que realizan la resta y multiplicación de dos números y devuelven el resultado. Ambos reciben (int,int) y devuelven también un entero (int). Realizo llamadas a estos métodos desde el main.

--- src/main/java/com/guillermopalazoncano/guillermo/git/GuillermoGIT.java
+++ src/main/java/com/guillermopalazoncano/guillermo/git/GuillermoGIT.java
@@ -15,6 +15,10 @@ public class GuillermoGIT {
         int valor2 = 3;
         System.out.println("El resultado de sumar "+valor1+" y "+valor2+" "
                 + "es: "+suma(valor1, valor2));
+        System.out.println("El resultado de multiplicar "+valor2+" y "+valor1+" "
+                + "es: "+multiplica(valor2, valor1));
+        System.out.println("El resultado de sumar "+valor2+" y "+valor1+" "
+                + "es: "+resta(valor2, valor1));
     }
     
     private static int suma (int num1, int num2){
@@ -22,4 +26,11 @@ public class GuillermoGIT {
         return resultado;
     }
     
+    private static int resta(int num1, int num2){
+        return num1-num2;
+    }
+    
+    private static int multiplica(int num1, int num2){
+        return num1*num2;
+    }
 }

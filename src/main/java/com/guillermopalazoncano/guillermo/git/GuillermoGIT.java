37d08d5de41f5cb36917ea92ad526e29a5a743d9
guillermopalazoncano <guillermopalazoncano@localhost>
2023-03-06 22:09:05 +0100

    Versión inicial del proyecto guillermo-GIT. Se sube todo el contenido del mismo.

---
+++ src/main/java/com/guillermopalazoncano/guillermo/git/GuillermoGIT.java
@@ -0,0 +1,15 @@
+package com.guillermopalazoncano.guillermo.git;
+
+/**
+ *
+ * @author guillermopalazoncano
+ */
+public class GuillermoGIT {
+
+    public static void main(String[] args) {
+        /*
+        Proyecto creado por Guillermo Palazón Cano para
+        la tarea de Entornos de Desarrollo
+        */
+    }
+}

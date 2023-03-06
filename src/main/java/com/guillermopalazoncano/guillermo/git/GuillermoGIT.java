65f7f59a72cbd77c9a6715620e2745e7719fb308
guillermopalazoncano <guillermopalazoncano@localhost>
2023-03-06 22:18:40 +0100

    Nuevo método que realiza la suma de dos números y devuelve el resultado. Recibe por tanto (int,int) y devuelve también un entero (int). Realizo una llamada a este método desde el método main.

--- src/main/java/com/guillermopalazoncano/guillermo/git/GuillermoGIT.java
+++ src/main/java/com/guillermopalazoncano/guillermo/git/GuillermoGIT.java
@@ -11,5 +11,15 @@ public class GuillermoGIT {
         Proyecto creado por Guillermo Palazón Cano para
         la tarea de Entornos de Desarrollo
         */
+        int valor1 = 5;
+        int valor2 = 3;
+        System.out.println("El resultado de sumar "+valor1+" y "+valor2+" "
+                + "es: "+suma(valor1, valor2));
     }
+    
+    private static int suma (int num1, int num2){
+        int resultado = num1+num2;
+        return resultado;
+    }
+    
 }
